813959dc7283ea21307dea6dd9ecdde7b619ff02
NandishDPatel <nandishdpatel22@gmail.com>
2024-12-07 16:05:09 -0600

    require('./models/User') error solving

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
-require('./models/user');
+require('./models/User');
 require('./Services/passport');
 const keys = require('./config/keys');
 
